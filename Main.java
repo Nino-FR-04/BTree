31a49d02608ad3b50a3a2240df5f4c3e8fa32bb2
Nino-FR-04 <davidfrx322@gmail.com>
2025-06-27 15:52:15 -0500

    Comentarios y correcciones menores

--- Main.java
+++ Main.java
@@ -1,21 +1,23 @@
+import java.util.ArrayList;
+
 import bTree.BTree;
 
 public class Main {
     public static void main(String[] args) {
-        // Crear un árbol B de orden 4 (máx 3 claves por nodo)
-        BTree<Integer> btree = new BTree<>(4);
+        
+        BTree<Integer> tree = new BTree<>(3);
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(3);
+        tree.insert(4);
+        tree.insert(5);
 
-        // Insertar claves
-        int[] keys = {10, 20, 5, 6, 12, 30, 7, 17};
-        for (int key : keys) {
-            System.out.println("Insertando: " + key);
-            btree.insert(key);
-        }
 
-        System.out.println(btree.search(10));
-        btree.remove(10);
-        System.out.println(btree.search(10));
-        System.out.println(btree.getRoot());
+        System.out.println(tree);
+
+        tree.remove(1);
+
+        System.out.println(tree);
         
     }
 }
